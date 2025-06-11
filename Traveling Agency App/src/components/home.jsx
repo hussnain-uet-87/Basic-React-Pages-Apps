5540c27b3ae718f9e6b97d0f2c6952d3a4329265
hussnain-uet-87 <hussnainuet87@gmail.com>
2025-06-11 03:43:35 -0700

    Add files via upload

--- Traveling Agency App/src/components/home.jsx
+++ Traveling Agency App/src/components/home.jsx
@@ -11,8 +11,8 @@ const Home = () => {
           take people
         </h1>
         <p>
-          To get the best of your adventure you just <br />
-          need to leave and go where you like.We are <br />
+          To get the best of your adventure you just 
+          need to leave and go where you like.We are 
           waiting for you!
         </p>
         <button>Plan a Trip <i class="bi bi-arrow-right"></i></button>
